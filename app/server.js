4e42234095d5428bc5046c1f825599c6dbf19eeb
NickMutemasango <geniusnickie@gmail.com>
2024-10-18 11:25:20 +0200

    new

--- app/server.js
+++ app/server.js
@@ -1,8 +1,7 @@
-// server.js
-const express = require('express');
-const nodemailer = require('nodemailer');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import express from 'express';
+import nodemailer from 'nodemailer';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(cors());
